1bb3fe3d31db147843e3919a80eeee6405e33ef5
shchedrin <ilya.shchedrin@gmail.com>
2023-11-30 04:04:59 +0300

    Enable job for updating payments status

--- app/Http/Controllers/Payments/PaymentsController.php
+++ app/Http/Controllers/Payments/PaymentsController.php
@@ -35,11 +35,14 @@ class PaymentsController extends Controller
             ->defaultSort('pay_date_oracle')
             ->allowedSorts([
                 'id', 'pay_event_id', 'account_id', 'amount', 'tender_source', 'tender_source_descr', 'filen_name',
-                'pay_date_oracle', 'fiscal_flag', 'file_name'
+                'pay_date_oracle', 'fiscal_flag', 'file_name', 'fiscal_number', 'shift_fiscal_number', 'receipt_date',
+                'fn_number', 'kkt_registration_number', 'fiscal_attribute', 'fiscal_doc_number'
             ])
             ->allowedFilters([
                 'id', 'pay_event_id', 'account_id', 'amount', 'tender_source', 'tender_source_descr', 'filen_name',
-                'pay_date_oracle', 'fiscal_flag', 'file_name', $globalSearch])
+                'pay_date_oracle', 'fiscal_flag', 'file_name', 'fiscal_number', 'shift_fiscal_number', 'receipt_date',
+                'fn_number', 'kkt_registration_number', 'fiscal_attribute', 'fiscal_doc_number', $globalSearch
+            ])
             ->paginate()
             ->withQueryString();
 
